e326e746ec566577b0c1d87ebd0e25f85d6f48a3
ProfessorKaos64 <mdeguzis@gmail.com>
2014-05-10 15:42:02 -0400

    testing

---
+++ update-git-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#small script to update configuration files for git repo
+#Version 1.1
+
+#Nestopia
+#default path: /home/$USER/.nestopia
+cp -Rvi $HOME/.nestopia/nstcontrols $HOME/RetroRig/Nestopia
+
