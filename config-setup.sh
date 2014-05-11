0e8816ac7d460f3663b1a0c5e0d7e87363da25b9
ProfessorKaos64 <mdeguzis@gmail.com>
2014-05-11 18:39:57 -0400

    gsettings screensaver, xbmc autostart, addon testing

--- config-setup.sh
+++ config-setup.sh
@@ -32,15 +32,15 @@ while true; do
 		#add repository for dolphin-emu
 		sudo add-apt-repository -y ppa:glennric/dolphin-emu
 		sudo apt-get update
-		sudo apt-get install -y xboxdrv zsnes nestopia pcsxr pcsx2:i386\
-		mame mupen64plus qjoypad xbmc dolphin-emu-master stella	
+		sudo apt-get install -y xboxdrv curl zsnes nestopia pcsxr pcsx2:i386\
+		mame mupen64plus dconf-tools qjoypad xbmc dolphin-emu-master stella	
 		#xbmc does not (at least for Ubuntu's repo pkg) load the
 		#dot files without loading XBMC at least once
 		#copy in default folder base from first run:
 		mkdir -pv $HOME/RetroRig/.xbmc/
-		cp -Rv $HOME/RetroRig/XBMC/* $HOME/.xbmc/
+		cp -Rv $HOME/RetroRig/XBMC/* $HOME/.xbmc/addons/
 		#This will be automated with curl later!
-		wget -o http://romcollectionbrowser.googlecode.com/			filesscript.games.rom.collection.browser-2.0.10.zip 
+		curl -O http://romcollectionbrowser.googlecode.com/filesscript.games.rom.collection.browser-2.0.10.zip $HOME/.xbmc/
 		echo ""
 		echo "RetroRig files cloned into: $HOME/RetroRig"	 
 		sleep 5s		
@@ -54,7 +54,10 @@ while true; do
 		#pull ROM Collection Browser into xbmc 
 		asfd
 		#disable screensaver, XBMC will manage this
-		sudo gsettings set org.gnome.settings-daemon.plugins.power active false
+		#export display to allow gsettings running in terminal window
+		export DISPLAY=:0.0
+		gsettings set org.gnome.settings-daemon.plugins.power active false
+		gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 		#setup skelton folders for XBMC Rom Collection Browser
 		#ROMs		
 		mkdir -pv $HOME/Games/ROMs/Atari\ 2600/
@@ -142,12 +145,16 @@ while true; do
 		cp -Rv /$HOME/RetroRig/Dolphin/GCPadNew.ini $HOME/.dolphin-emu/Config/
 		#Wii controller config
 		#[PENDING]
+
 		#opengl config
 		cp -Rv /$HOME/RetroRig/Dolphin/gfx_opengl.ini $HOME/.dolphin-emu/Config/
 
 		#copy configs for other utilities
 		cp -v $HOME/RetroRig/controller-cfg/qjoypad_launch.sh $HOME/Games/Configs/
 		cp -v $HOME/RetroRig/controller-cfg/xpad-wireless.xboxdrv $HOME/Games/Configs/
+
+		#create autostart for XBMC
+		sudo cp -v /usr/bin/xbmc /etc/xdg/autostart/
 
 		#clear and prompt
 	        ;;
