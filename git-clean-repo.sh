ac4e52865523784ef11bf38ff56fd0769253dbb6
ProfessorKaos64 <mdeguzis@gmail.com>
2014-05-16 06:52:43 -0400

    repo clean config update
    Former-commit-id: 025fd471c28cfbff599592699a09ff0e287d260f

--- git-clean-repo.sh
+++ git-clean-repo.sh
@@ -62,7 +62,8 @@ read folder
 
 echo $folder
 
-git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch $folder' --prune-empty -f -- --all 
+echo "git filter-branch --tag-name-filter cat --index-filter 'git rm -r --cached --ignore-unmatch $folder' --prune-empty -f -- --all "
+sleep 10s
 
 #This command is adapted from other sources—the
 #principle addition is --tag-name-filter cat which ensures tags are rewritten as well.
