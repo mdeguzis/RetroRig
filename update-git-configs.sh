d4287e39d4f6cfe91ac81edaad04c014087bc083
ProfessorKaos64 <mdeguzis@gmail.com>
2014-05-15 22:38:53 -0400

    added gc function to .git folder, roadmap
    Former-commit-id: 019117e1705a3ce353d3fd2ff8f7634d85573e71

--- update-git-configs.sh
+++ update-git-configs.sh
@@ -80,6 +80,19 @@ cp -Rv $HOME/.xbmc/userdata/addon_data $HOME/RetroRig/XBMC/userdata
 #xboxdrv service
 cp -v /etc/init.d/xboxdrv $HOME/RetroRig/init-scripts/
 
+
+#Prune garbage files:
+
+#standard small command
+#git gc
+#optional: prune data to avoid huge .git (when needed)
+#git gc --aggressive --prune
+
+#From Linus:
+#The equivalent of "git gc --aggressive" - but done *properly* - is to do (overnight) something like
+git repack -a -d --depth=250 --window=250
+
+#commit data
 git add *
 git commit
 git push origin master
