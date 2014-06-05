7a988eb747bf3557a974886e08b64943d8712461
ProfessorKaos64 <mdeguzis@gmail.com>
2014-06-04 21:28:52 -0400

    Day changes of script restructuring part 2
    Former-commit-id: b9f1ca7e0665416e79fc17ace8c5a5b9a20ce02b

--- retrorig_setup.sh
+++ retrorig_setup.sh
@@ -8,20 +8,134 @@
 # You can also reach me on twitter: @N3RD42
 #
 
-#remove previous install log to start fresh
-rm -f install_log.txt
-#remove previous uninstall log, if any
-rm -f uninstall_log.txt
 
-#start logging
-echo "-----------------------------------------------------------" >> install_log.txt
-echo "Starting install log..." >> install_log.txt
-echo "-----------------------------------------------------------" >> install_log.txt
+#set up errmsgs and postmsgs
+__ERRMSGS=""
+__postMSGs=""
+
+######################################
+# Start Helper Functions
+######################################
+
+#set -o nounset
+
+function getScriptAbsoluteDir() {
+    # @description used to get the script path
+    # @param $1 the script $0 parameter
+    local script_invoke_path="$1"
+    local cwd=`pwd`
+
+    # absolute path ? if so, the first character is a /
+    if test "x${script_invoke_path:0:1}" = 'x/'
+    then
+	RESULT=`dirname "$script_invoke_path"`
+    else
+	RESULT=`dirname "$cwd/$script_invoke_path"`
+    fi
+}
+
+function import() {
+    
+    # @description importer routine to get external functionality.
+    # @description the first location searched is the script directory.
+    # @description if not found, search the module in the paths contained in $SHELL_LIBRARY_PATH environment variable
+    # @param $1 the .shinc file to import, without .shinc extension
+    module=$1
+
+    if test "x$module" == "x"
+    then
+	echo "$script_name : Unable to import unspecified module. Dying."
+        exit 1
+    fi
+
+	if test "x${script_absolute_dir:-notset}" == "xnotset"
+    then
+	echo "$script_name : Undefined script absolute dir. Did you remove getScriptAbsoluteDir? Dying."
+        exit 1
+    fi
+
+	if test "x$script_absolute_dir" == "x"
+    then
+	echo "$script_name : empty script path. Dying."
+        exit 1
+    fi
+
+    if test -e "$script_absolute_dir/$module.shinc"
+    then
+        # import from script directory
+        . "$script_absolute_dir/$module.shinc"
+        echo "Loaded module $script_absolute_dir/$module.shinc"
+        return
+    elif test "x${SHELL_LIBRARY_PATH:-notset}" != "xnotset"
+    then
+        # import from the shell script library path
+        # save the separator and use the ':' instead
+        local saved_IFS="$IFS"
+        IFS=':'
+        for path in $SHELL_LIBRARY_PATH
+        do
+    if test -e "$path/$module.shinc"
+            then
+                . "$path/$module.shinc"
+                return
+    fi
+done
+        # restore the standard separator
+        IFS="$saved_IFS"
+    fi
+    echo "$script_name : Unable to find module $module."
+    exit 1
+}
+
+
+function loadConfig()
+{
+    # @description Routine for loading configuration files that contain key-value pairs in the format KEY="VALUE"
+    # param $1 Path to the configuration file relate to this file.
+    local configfile=$1
+    if test -e "$script_absolute_dir/$configfile"
+    then
+        . "$script_absolute_dir/$configfile"
+        echo "Loaded configuration file $script_absolute_dir/$configfile"
+        return
+    else
+	echo "Unable to find configuration file $script_absolute_dir/$configfile"
+        exit 1
+    fi
+}
+
+script_invoke_path="$0"
+script_name=`basename "$0"`
+getScriptAbsoluteDir "$script_invoke_path"
+script_absolute_dir=$RESULT
+
+# load script modules
+import "scriptmodules/helpers"
+import "scriptmodules/configuration"
+import "scriptmodules/menus"
+import "scriptmodules/settings"
+import "scriptmodules/setup"
+import "scriptmodules/gamepads"
 
 ######################################
 # Start main script
 ######################################
 
+scriptdir=`dirname $0`
+scriptdir=`cd $scriptdir && pwd`
+
+#check for pre-requisites
+rrs_prereq
+
+if [[ "$1" == "--help" ]]; then
+    rrs_showHelp
+    exit 0
+fi
+
+if [ $(id -u) -ne 0 ]; then
+    printf "Script must be run as root. Try 'sudo ./retropie_setup' or ./retropie_setup --help for further information\n"
+    exit 1
+fi
 
 while true; do
 cmd=(dialog --backtitle "LibreGeek.org RetroRig Installer" --menu "| Main Menu | \
@@ -135,94 +249,4 @@ options=(1 "Install Software"
 		break
 	fi
 done
-}
-
-function _update-git () {
-	clear
-	echo "-----------------------------------------------------------" |tee -a install_log.txt
-	echo "Updating git repo" | tee -a install_log.txt
-	echo "-----------------------------------------------------------" | tee -a install_log.txt
-	sleep 2s
-	cd $HOME/RetroRig/
-	git pull
-	#pause for reviewing changes
-	sleep 5s
-	
-	#clear
-	clear
-
-	bash config-setup.sh
-}
-
-function _update-binaries () {
-	clear
-	echo "-----------------------------------------------------------" |tee -a install_log.txt
-	echo "Updating emulator binaries" | tee -a install_log.txt
-	echo "-----------------------------------------------------------" | tee -a install_log.txt
-	apt-get install -y xboxdrv curl zsnes nestopia pcsxr pcsx2:i386 \
-	python-software-properties pkg-config software-properties-common mess \
-	mame mupen64plus dconf-tools mednafen qjoypad xbmc dolphin-emu-master stella \
-	build-essential | tee -a install_log.txt	
-	sleep 3s
-	#clear
-	clear
-}
-
-function _upgrade-system () {
-	clear
-	echo "-----------------------------------------------------------" |tee -a install_log.txt
-	echo "Upgrading system" | tee -a install_log.txt
-	echo "-----------------------------------------------------------" | tee -a install_log.txt
-	apt-get update
-	apt-get upgrade
-	sleep 3s
-	#clear
-	clear
-}
-
-function _start-xbmc () {
-	dialog --infobox "starting RetroRig" 3 21
-	sleep 1s
-	xbmc
-	#clear
-	clear
-}
-
-function _reboot () {
-
-	data=$(tempfile 2>/dev/null)
- 
-	# trap it
-	trap "rm -f $data" 0 1 2 5 15
-	 
-	# get password with the --insecure option
-	dialog --title "Enter password to confirm!" \
-	--clear \
-	--insecure \
-	--passwordbox "Enter your user password" 7 28 2> $data
-	 
-	ret=$?
-       #Exit status is subject to being overridden  by  environment  variables.
-       #Normally they are:
-       #0    if dialog is exited by pressing the Yes or OK button.
-       #1    if the No or Cancel button is pressed.
-       #2    if the Help button is pressed.
-       #3    if the Extra button is pressed.
-       #-1   if  errors occur inside dialog or dialog is exited by pressing the
-       #     ESC key.
-
-	case $ret in
-	  0)
-	    dialog --infobox "Rebooting PC" 3 0 ; sleep 2s
-	    sleep 5s
-	    /sbin/reboot
-	    ;;
-	  1)
-	    dialog --infobox "Cancel Pressed" 3 18 ; sleep 2s
-	    sleep 2s
-	    ;;
-	  255)
-	    [ -s $data ] &&  cat $data || echo "ESC pressed.";;
-	esac
-
 }
