eab82b85d393e2e9b47d10b760fe293d8b2ab37c
ProfessorKaos64 <mdeguzis@gmail.com>
2014-06-22 16:11:04 -0400

    inital large testing of new xbmc build

--- init-scripts/ps3_blu_controller/ps3_autodetect_xbmc.sh
+++ init-scripts/ps3_blu_controller/ps3_autodetect_xbmc.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-clear
-echo "Recconecting PS3 Controller for XBMC.."
-sleep 2s
+# Project RetroRig: https://github.com/ProfessorKaos64/RetroRig
+# This script detects a change on available PS controller. If detected, it notifies XBMC.
+# Author: Jens-Christiansen, aka  beaumanvienna/JC
+# Revision: 2014/06/21, initial version
 
-count=0
 
-while ! grep -q "PLAYSTATION(R)3" /proc/bus/input/devices
+controllerAvailable=`cat /proc/bus/input/devices | grep PLAYSTATION`
+echo $controllerAvailable
+
+while [ 0 ]
 do
-	# not set, echo below fore TESTING ONLY!!!
-	# set ps3_blu_complete to 0 for false
-	echo "Your controller is not turned on! please press the PS3 Button!"
-	sleep 3s
+  oldControllerAvailable=$controllerAvailable
+  controllerAvailable=`cat /proc/bus/input/devices | grep PLAYSTATION`
+  if [ "$controllerAvailable" != "$oldControllerAvailable" ]; then
+    killall xbmc.bin -SIGUSR1
+  fi
+  sleep 1
 done
-
-# give the controller about 3 seconds to start
-sleep 3s
-# fork to background
-echo starting xbmc
-sleep 1s
-xbmc
