38d077c74320203ce18ae12b689b9c046b4ae464
ProfessorKaos64 <mdeguzis@gmail.com>
2014-07-27 14:08:20 -0400

    updated gp_autodetect_xbmc.sh for gamepad type swaps

--- XBMC-cfgs/extra/gp_autodetect_xbmc.sh
+++ XBMC-cfgs/extra/gp_autodetect_xbmc.sh
@@ -2,56 +2,73 @@
 
 # Project RetroRig: https://github.com/ProfessorKaos64/RetroRig
 # This script detects a change on available PS controller. If detected, it notifies XBMC.
-# Author: Jens-Christiansen, aka  beaumanvienna/JC
+# Authors: Jens-Christiansen, Michael DeGuzis
 # Revision: 2014/06/21, initial version
+# Revision: 2014/07/26, by pk, added checks for all controllers
 
-# echo controllers
-controllerAvailable_ps3_blu=`cat /proc/bus/input/devices | grep PLAYSTATION`
-echo "Available PS3 Controllers (Bluetooth):"
-echo $controllerAvailable_ps3_blu
+###################################################
+# set controller type based on RetroRig Script
+###################################################
 
-controllerAvailable_ps3_usb=`cat /proc/bus/input/devices | grep PLAYSTATION`
-echo "Available PS3 Controllers (USB):"
-echo $controllerAvailable_ps3_usb
+controllertype="controller_temp"
 
-controllerAvailable_x360_wireless=`cat /proc/bus/input/devices | grep "Xbox Gamepad (userspace driver)"`
-echo "Available Xbox 360 Controllers (Wireless):"
-echo $controllerAvailable_x360_wireless
-l
-controllerAvailable_x360_usb=`cat /proc/bus/input/devices | grep "Xbox Gamepad (userspace driver)"`
-echo "Available Xbox 360 Controllers (Wired):"
-echo $controllerAvailable_x360_usb
+###################################################
 
 while [ 0 ]
 do
 
-  # check for PS3 Bluetooth Controller
-  oldControllerAvailable=$controllerAvailable
-  controllerAvailable=`cat /proc/bus/input/devices | grep PLAYSTATION`
-  if [ "$controllerAvailable" != "$oldControllerAvailable" ]; then
-    killall xbmc.bin -SIGUSR1
-  fi
+# check for PS3 Bluetooth Controller
+if [ "$controllertype" = "ps3_blu" ]; then
 
-  # check for PS3 USB Controller
-  oldControllerAvailableUSB=$controllerAvailableUSB
-  controllerAvailableUSB=`lsusb | grep PlayStation`
-  if [ "$controllerAvailableUSB" != "$oldControllerAvailableUSB" ]; then
-    killall xbmc.bin -SIGUSR1
-  fi
+          oldControllerAvailable=$controllerAvailable
+          controllerAvailable=`cat /proc/bus/input/devices | grep "PLAYSTATION(R)"`
+          if [ "$controllerAvailable" != "$oldControllerAvailable" ]; then
+            killall xbmc.bin -SIGUSR1
+          fi
+fi
 
-  # check for Xbox360 USB Hub (wireless) Controller
-  oldControllerAvailable=$controllerAvailable_x360_wireless
-  controllerAvailableU=`lsusb | grep Xbox 360 Wireless`
-  if [ "$controllerAvailable_x360_wireless" != "$oldControllerAvailable" ]; then
-    killall xbmc.bin -SIGUSR1
-  fi
+# check for PS3 USB Controller
 
-  # check for Xbox360 USB connected Controller
-  oldControllerAvailableUSB=$controllerAvailable_x360_usb
-  controllerAvailableUSB=`lsusb | grep Xbox360 Controller`
-  if [ "$controllerAvailableUSB" != "$oldControllerAvailableUSB" ]; then
-    killall xbmc.bin -SIGUSR1
-  fi
+if [ "$controllertype" = "ps3_usb" ]; then
+          # check for PS3 USB Controller
+          oldControllerAvailableUSB=$controllerAvailableUSB
+          controllerAvailableUSB=`lsusb | grep -E "PlayStation 3 Controller"`
+          if [ "$controllerAvailableUSB" != "$oldControllerAvailableUSB" ]; then
+            echo "Restarting xboxdrv services"
+            sudo /usr/sbin/service xboxdrv restart
+            killall xbmc.bin -SIGUSR1
+          fi
+ fi
+
+# check for xbox 360 wirelesss Controller
+
+if [ "$controllertype" = "xbox360_wireless" ]; then
+          # check for Xbox360 USB Hub (wireless) Controller
+          oldControllerAvailable=$controllerAvailable
+          controllerAvailable=`lsusb | grep Xbox 360 Wireless`
+          if [ "$controllerAvailable" != "$oldControllerAvailable" ]; then
+            echo "Restarting xboxdrv services"
+            sudo /usr/sbin/service xboxdrv restart
+            killall xbmc.bin -SIGUSR1
+          fi
+ fi
+
+### XBOX 360 WIRED HOTPLUGGING BELOW IS NOT CURRENTLY FUNCTIONING!!! ###
+
+# check for xbox 360 wired Controller
+if [ "$controllertype" = "x360_wired" ]; then
+
+          # check for Xbox360 USB connected Controller
+          oldControllerAvailableUSB=$controllerAvailableUSB
+          controllerAvailableUSB=`lsusb | grep "Xbox360 Controller"`
+          if [ "$controllerAvailableUSB" != "$oldControllerAvailableUSB" ]; then
+            echo "Restarting xboxdrv services"
+            killall xboxdrv
+            sudo /usr/sbin/service xboxdrv restart
+            killall xbmc.bin -SIGUSR1
+          fi
+fi
 
   sleep 1
 done
+
