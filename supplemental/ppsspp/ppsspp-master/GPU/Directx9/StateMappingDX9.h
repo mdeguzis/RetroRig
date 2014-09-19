453deb5a1f292ceab1249e1749cf3f3e7efd5d53
ProfessorKaos64 <mdeguzis@gmail.com>
2014-09-19 19:48:20 -0400

    prepping a few ppsspp build files

--- supplemental/ppsspp/ppsspp-master/GPU/Directx9/StateMappingDX9.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "helper/global.h"
-#include "helper/dx_state.h"
-//#include "../native/gfx/gl_common.h"
--- supplemental/ppsspp/ppsspp-master/UI/BackgroundAudio.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-void SetBackgroundAudioGame(const std::string &path);
-int MixBackgroundAudio(short *buffer, int size);
