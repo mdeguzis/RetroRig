82de0f0f51eba57fca121773386e91b6dcf02553
beaumanvienna <jc.lache@gmail.com>
2014-10-28 21:05:50 +0100

    auto start function

--- XBMC-cfgs/extra/gp_autodetect_xbmc.sh
+++ XBMC-cfgs/extra/gp_autodetect_xbmc.sh
@@ -14,7 +14,22 @@ do
   oldControllerAvailable=$controllerAvailable
   controllerAvailable=`cat /proc/bus/input/devices | grep PLAYSTATION`
   if [ "$controllerAvailable" != "$oldControllerAvailable" ]; then
+  
+    echo "sending gamepad reconfiguration request"
     killall xbmc.bin -SIGUSR1
+  
+    #auto start xbmc for first controller to be switched on
+    if [ -z "$oldControllerAvailable" ]; then
+      #a controller was switched on
+      if [ -z "$(pidof xbmc.bin 2>/dev/null)" ]; then
+        # xbmc not running yet
+        /usr/share/applications/startXBMC.sh
+        
+        #make sure it knows of the controller just beeing switched on
+        sleep 5
+        killall xbmc.bin -SIGUSR1
+      fi
+    fi
   fi
   sleep 1
 done
